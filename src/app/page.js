79c327761938887b3d2ed9de4a2deaf525fea4b2
yogaviveka <142080646+Xannydayo@users.noreply.github.com>
2024-03-16 14:19:26 +0700

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import HeroSection from "@/app/components/HeroSection";
+import Navbar from "@/app/components/navbar";
+import AboutSection from "@/app/components/AboutSection";
+import ProjectsSection from "@/app/components/ProjectSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AchievementsSection />
+        <AboutSection />
+        <ProjectsSection />
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
